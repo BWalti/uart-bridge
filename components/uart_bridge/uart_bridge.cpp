924e8139cca470e21f534faff8f60f5005d602b5
Beat Walti <beat_walti@epam.com>
2025-02-19 18:03:56 +0100

    fix: using proper ESPHome UART

--- components/uart_bridge/uart_bridge.cpp
+++ components/uart_bridge/uart_bridge.cpp
@@ -1,5 +1,6 @@
 #include "uart_bridge.h"
 #include "esphome/core/log.h"
+#include "esphome/components/uart/uart_component.h"
 
 namespace esphome {
 namespace uart_bridge {
@@ -8,22 +9,25 @@ static const char *TAG = "uart_bridge";
 
 void UARTBridge::setup() {
   ESP_LOGCONFIG(TAG, "Setting up UART Bridge...");
+  
+  // Configure USB CDC UART (ESP32-C3's built-in USB serial)
+  uart::UARTComponent::setup_usb_cdc();
 }
 
 void UARTBridge::loop() {
-  // Handle data from USB to UART
-  while (Serial.available()) {
-    int c = Serial.read();
-    if (c >= 0) {
-      this->uart_parent_->write_byte(c);
-    }
+  uint8_t buf[64];
+  size_t len;
+
+  // Handle data from USB CDC to UART
+  if ((len = uart::UARTComponent::read_usb_cdc(buf, sizeof(buf))) > 0) {
+    this->uart_parent_->write_array(buf, len);
   }
 
-  // Handle data from UART to USB
-  while (this->uart_parent_->available()) {
-    int c = this->uart_parent_->read();
-    if (c >= 0) {
-      Serial.write(c);
+  // Handle data from UART to USB CDC
+  if (this->uart_parent_->available() > 0) {
+    len = this->uart_parent_->read_array(buf, sizeof(buf));
+    if (len > 0) {
+      uart::UARTComponent::write_usb_cdc(buf, len);
     }
   }
 }

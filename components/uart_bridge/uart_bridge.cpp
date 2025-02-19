b466cf4f8f7498e4c0483f095c229682ff37e6fc
Beat Walti <beat_walti@epam.com>
2025-02-19 18:23:15 +0100

    fix: ensure that host does not get re-flashed

--- components/uart_bridge/uart_bridge.cpp
+++ components/uart_bridge/uart_bridge.cpp
@@ -1,6 +1,7 @@
 #include "uart_bridge.h"
 #include "esphome/core/log.h"
-#include "driver/usb_serial_jtag.h"
+#include "driver/gpio.h"
+#include "esp_private/usb_phy.h"
 
 namespace esphome {
 namespace uart_bridge {
@@ -10,29 +11,26 @@ static const char *TAG = "uart_bridge";
 void UARTBridge::setup() {
   ESP_LOGCONFIG(TAG, "Setting up UART Bridge...");
   
-  // Initialize USB Serial JTAG driver
-  usb_serial_jtag_driver_config_t config = {
-    .tx_buffer_size = 256,
-    .rx_buffer_size = 256,
+  // Explicitly disable USB CDC JTAG
+  usb_phy_config_t phy_config = {
+    .controller = USB_PHY_CTRL_SERIAL_JTAG,
+    .target = USB_PHY_TARGET_EXT,
+    .d_pin = -1,
+    .d_pin_func = -1,
   };
-  ESP_ERROR_CHECK(usb_serial_jtag_driver_install(&config));
+  usb_phy_disable();
+  ESP_ERROR_CHECK(usb_new_phy(&phy_config));
 }
 
 void UARTBridge::loop() {
   uint8_t buf[64];
   size_t len;
 
-  // Handle data from USB to UART
-  len = usb_serial_jtag_read_bytes(buf, sizeof(buf), 0);
-  if (len > 0) {
-    this->uart_parent_->write_array(buf, len);
-  }
-
-  // Handle data from UART to USB
+  // Handle data from UART to UART
   if (this->uart_parent_->available() > 0) {
     len = this->uart_parent_->read_array(buf, sizeof(buf));
     if (len > 0) {
-      usb_serial_jtag_write_bytes(buf, len, 0);
+      this->uart_parent_->write_array(buf, len);
     }
   }
 }
